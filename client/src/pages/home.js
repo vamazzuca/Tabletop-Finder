45ea8b1bf19ee7010dc472b52b21a46ab0fc21f6
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2023-09-18 06:26:36 -0600

    Started Styling Home Page

---
+++ client/src/pages/home.js
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <div className="h-screen">
+            <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+                <div className="grid grid-cols-4 h-full">
+                    <h1 className="text-3xl text-blue-500 font-bold">
+                        Hello world!
+                    </h1>
+                </div>
+            </div>
+        </div>
+      
+    );
+  }
+  
+  export default Home;
