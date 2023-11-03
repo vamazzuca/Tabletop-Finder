32eb096b466b309f2038eaecf5c95758c2c7fb5c
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2023-11-03 04:17:22 -0600

    Set up Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk';
+import reducers from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ import postRoutes from "./routes/posts.js"
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Alvrrick:8hqYv95B5WL4mKvG@cluster0.xmbppp6.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
