11f51df8eabf598f09ac431a096565a722536433
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2024-07-08 14:08:57 -0600

    Update chatbox.js

--- client/src/components/chat/chatbox.js
+++ client/src/components/chat/chatbox.js
@@ -11,7 +11,7 @@ import { Markup } from 'interweave'
 import { leaveEvent } from "../../actions/posts";
 import { leaveChat } from "../../actions/chats";
 
-const ENDPOINT = "wss://tableserver.vittoriomazzuca.ca";
+const ENDPOINT = "https://tableserver.vittoriomazzuca.ca";
 var socket, selectedChatCompare;
 
 function ChatBox({chatid}) {
